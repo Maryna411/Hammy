01aa5ebe0d52a06ac97956d5c75f78e00d44052f
Maryna411 <marynagctc@gmail.com>
2026-07-20 18:51:09 +0300

    Update Hammy.tsx

--- components/Hammy.tsx
+++ components/Hammy.tsx
@@ -8,29 +8,18 @@ interface Props {
   className?: string;
 }
 
-const POSITION: Record<HammyMood, string> = {
-  idle: "3% 0%",
-  listening: "50% 0%",
-  thinking: "97% 0%",
-  happy: "3% 74%",
-  oops: "50% 74%",
-};
-
 export default function Hammy(props: Props) {
   const mood = props.mood || "idle";
   const size = props.size || 64;
   const extraClass = props.className || "";
-  const label = "Hammy mood: " + mood;
-  const wrapperClass = "hammy-breathe inline-block overflow-hidden rounded-full " + extraClass;
+  const wrapperClass = "hammy-breathe inline-block " + extraClass;
 
-  const style = {
-    width: size,
-    height: size,
-    backgroundImage: "url(/Hammy.png)",
-    backgroundSize: "330% 310%",
-    backgroundPosition: POSITION[mood],
-    backgroundRepeat: "no-repeat",
-  };
-
-  return <span role="img" aria-label={label} className={wrapperClass} style={style}></span>;
+  return (
+    <img
+      src={"/" + mood + ".png"}
+      alt={"Hammy mood: " + mood}
+      className={wrapperClass}
+      style={{ width: size, height: size, objectFit: "contain" }}
+    />
+  );
 }
